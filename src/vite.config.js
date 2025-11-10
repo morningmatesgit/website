e0c7a8fbfa12560a298d62a30dbbc8ceabeb9f91
Nitheeshkumar13 <nitheeshk481@gmail.com>
2025-11-10 20:20:32 +0530

    updated vite.config.js

--- src/vite.config.js
+++ src/vite.config.js
@@ -2,23 +2,27 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    allowedHosts: [
-      'web.morningmates.in',
-      'morningmates.in',
-      'localhost',
-    ],
+ plugins: [react()],
+ server: {
+  host: '0.0.0.0',
+  port: 5173,
+  allowedHosts: [
+   'web.morningmates.in',
+   'morningmates.in',
+   'localhost',
+  ],
+ },
+ build: {
+  rollupOptions: {
+   external: [],
   },
-  build: {
-    rollupOptions: {
-      external: [],
+    // This is the CRITICAL block you added:
+    commonjsOptions: {
+        include: [/node_modules/],
     },
-  },
-  optimizeDeps: {
-    include: ['@emailjs/browser'],
-  },
+ },
+ optimizeDeps: {
+  include: ['@emailjs/browser'],
+ },
 })
 
